cc66402380ecf44c59799c46b36ffc141eb524ef
AlexCSS <kalyuzhnyi@gmail.com>
2020-05-08 21:28:59 +0300

    nav

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 ;(function() {
 	"use strict";
- 
+	
+	const menuToggle = document.querySelectorAll('.ba-menu-toggle');
+	const mainContent = document.querySelector('.ba-main-content');
+
+	menuToggle.forEach(element => {
+
+		element.addEventListener('click', () => {
+			mainContent.classList.toggle('shifted');
+		})
+
+	});
+
+
 })();
 
